2e86e3cc6b0bfab390c4536a7d67d26dbecbd66f
olegwn <olegwn@gmail.com>
2018-04-04 06:28:57 +0300

    React Navigation

--- app/index.js
+++ app/index.js
@@ -2,13 +2,31 @@ import React, { Component } from 'react';
 import { Platform } from 'react-native';
 import { Provider } from 'react-redux';
 import firebase from 'react-native-firebase';
+import { StackNavigator } from 'react-navigation';
 
 import configureStore from './config/configure-store';
 import Main from './containers';
 
 const store = configureStore();
+type Props = {
+};
 
-export default class App extends Component {
+class MainScreen extends Component<Props> {
+  render() {
+    return (
+      <Main />
+    );
+  }
+}
+
+const RootStack = StackNavigator({
+  Home: {
+    screen: MainScreen,
+  },
+});
+
+
+export default class App extends Component<Props> {
   componentDidMount() {
     firebase.analytics().logEvent('App_Started', {
       platform: Platform.OS,
@@ -18,7 +36,7 @@ export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Main />
+        <RootStack />
       </Provider>
     );
   }
