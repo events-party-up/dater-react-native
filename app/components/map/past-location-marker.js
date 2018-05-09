e4d3cd8102b36333e3b65625db87312a2f29ca8c
Oleg <olegwn@gmail.com>
2018-05-09 10:32:20 +0300

    Working version: refactoring

--- app/components/map/past-location-marker.js
+++ app/components/map/past-location-marker.js
@@ -4,10 +4,6 @@ import MapboxGL from '@mapbox/react-native-mapbox-gl';
 import { GeoCoordinates } from '../../types';
 import GeoUtils from '../../utils/geo-utils';
 
-const triangleLatDelta = 0.000005;
-const triangleLngDelta = 0.00001;
-const lineDelta = 0.000001;
-
 type Props = {
   pastCoords: Array<GeoCoordinates>,
   uid: string;
@@ -17,145 +13,26 @@ type Props = {
 class PastLocationMarker extends React.Component<Props> {
   mapStyles = MapboxGL.StyleSheet.create({
     path: {
-      // fillAntialias: true,
+      fillAntialias: true,
       fillColor: this.props.mode === 'own' ? 'rgba(128, 128, 128, 1)' : 'rgba(0, 128, 0, 1)',
       // fillOutlineColor: 'black',
-      // fillOpacity: 0.84,
+      fillOpacity: 0.84,
     },
   });
 
   render() {
     if (this.props.pastCoords.length < 2) return null;
 
-    const lines = this.props.pastCoords.slice(1).map((coords, index) => {
-      if (index === (this.props.pastCoords.length - 2)) {
-        const previousCoords = this.props.pastCoords[index];
-      }
+    const arrows = this.props.pastCoords.slice(1).map((coords, index) => {
+      const { heading, distance } = coords;
+      const arrowLength = 7;
 
-      return {
-        type: 'Feature',
-        id: `line-${index}`,
-        geometry: {
-          type: 'Polygon',
-          coordinates: [[
-            // line
-            [
-              this.props.pastCoords[index].longitude + lineDelta,
-              this.props.pastCoords[index].latitude + lineDelta,
-            ],
-            [
-              this.props.pastCoords[index].longitude - lineDelta,
-              this.props.pastCoords[index].latitude - lineDelta,
-            ],
-            [
-              coords.longitude - lineDelta,
-              coords.latitude - lineDelta,
-            ],
-            [
-              coords.longitude + lineDelta,
-              coords.latitude + lineDelta,
-            ],
-            [
-              this.props.pastCoords[index].longitude + lineDelta,
-              this.props.pastCoords[index].latitude + lineDelta,
-            ],
-            [
-              this.props.pastCoords[index].longitude + lineDelta,
-              this.props.pastCoords[index].latitude + lineDelta,
-            ],
-          ]],
-        },
-      };
-    });
-
-    const arrows = this.props.pastCoords.slice(1).map((coords, index) => (
-      {
-        type: 'Feature',
-        id: `arrow-${index}`,
-        geometry: {
-          type: 'Polygon',
-          coordinates: [[
-            // arrow
-            [
-              coords.longitude,
-              coords.latitude,
-            ],
-            [
-              this.props.pastCoords[index].longitude + triangleLngDelta,
-              this.props.pastCoords[index].latitude + triangleLatDelta],
-            [
-              this.props.pastCoords[index].longitude - triangleLngDelta,
-              this.props.pastCoords[index].latitude - triangleLatDelta,
-            ],
-            [
-              coords.longitude,
-              coords.latitude,
-            ],
-          ]],
-        },
-      }));
-
-    // const newArrows = this.props.pastCoords.slice(1).map((coords, index) => {
-    //   const startLat = this.props.pastCoords[index].latitude;
-    //   const startLon = this.props.pastCoords[index].longitude;
-    //   const startLatRad = GeoUtils.toRad(startLat);
-    //   const startLonRad = GeoUtils.toRad(startLon);
-    //   const { distance, heading } = coords;
-    //   const earthR = 6371000;
-
-    //   const endLatRad1 = (Math.asin(Math.sin(startLatRad) * Math.cos(distance / earthR)) +
-    //     (Math.cos(startLatRad) * Math.sin(distance / earthR) * Math.cos(GeoUtils.toRad(heading - 5))));
-    //   const endLonRad1 = startLonRad + Math.atan2(
-    //     Math.sin(GeoUtils.toRad(heading - 5)) * Math.sin(distance / earthR) * Math.cos(startLatRad),
-    //     Math.cos(distance / earthR) - (Math.sin(startLatRad) * Math.sin(endLatRad1)),
-    //   );
-
-    //   const endLatRad2 = (Math.asin(Math.sin(startLatRad) * Math.cos(distance / earthR)) +
-    //     (Math.cos(startLatRad) * Math.sin(distance / earthR) * Math.cos(GeoUtils.toRad(heading + 5))));
-    //   const endLonRad2 = startLonRad + Math.atan2(
-    //     Math.sin(GeoUtils.toRad(heading + 5)) * Math.sin(distance / earthR) * Math.cos(startLatRad),
-    //     Math.cos(distance / earthR) - (Math.sin(startLatRad) * Math.sin(endLatRad2)),
-    //   );
-    //   console.log(`distance: ${distance} heading: ${heading}`);
-
-    //   return {
-    //     type: 'Feature',
-    //     id: `arrow-${index}`,
-    //     geometry: {
-    //       type: 'Polygon',
-    //       coordinates: [[
-    //         // arrow
-    //         [
-    //           this.props.pastCoords[index].longitude,
-    //           this.props.pastCoords[index].latitude,
-    //         ],
-    //         [
-    //           GeoUtils.toDeg(endLonRad1),
-    //           GeoUtils.toDeg(endLatRad1),
-    //         ],
-    //         // [
-    //         //   coords.longitude,
-    //         //   coords.latitude,
-    //         // ],
-    //         [
-    //           GeoUtils.toDeg(endLonRad2),
-    //           GeoUtils.toDeg(endLatRad2),
-    //         ],
-    //         [
-    //           this.props.pastCoords[index].longitude,
-    //           this.props.pastCoords[index].latitude,
-    //         ],
-    //       ]],
-    //     },
-    //   };
-    // });
-
-    const newArrows2 = this.props.pastCoords.slice(1).map((coords, index) => {
-      const { heading } = coords;
-      const distance = 7;
-      console.log(`distance: ${distance} heading: ${heading}`);
-      const destinationPoint1 = GeoUtils.destinationPoint(coords, -distance, heading + 4);
-      const destinationPoint2 = GeoUtils.destinationPoint(coords, -distance, heading - 4);
+      const destinationPoint1 = GeoUtils.destinationPoint(coords, -arrowLength, heading + 4);
+      const destinationPoint2 = GeoUtils.destinationPoint(coords, -arrowLength, heading - 4);
+      const destinationPoint3 = GeoUtils.destinationPoint(coords, -arrowLength, heading + 1);
+      const destinationPoint4 = GeoUtils.destinationPoint(coords, -arrowLength, heading - 1);
+      const destinationPoint5 = GeoUtils.destinationPoint(coords, -distance, heading + 1);
+      const destinationPoint6 = GeoUtils.destinationPoint(coords, -distance, heading - 1);
       return {
         type: 'Feature',
         id: `arrow-${index}`,
@@ -175,6 +52,26 @@ class PastLocationMarker extends React.Component<Props> {
               destinationPoint1.latitude,
             ],
             [
+              destinationPoint3.longitude,
+              destinationPoint3.latitude,
+            ],
+            [
+              destinationPoint5.longitude,
+              destinationPoint5.latitude,
+            ],
+            [
+              destinationPoint5.longitude,
+              destinationPoint5.latitude,
+            ],
+            [
+              destinationPoint6.longitude,
+              destinationPoint6.latitude,
+            ],
+            [
+              destinationPoint4.longitude,
+              destinationPoint4.latitude,
+            ],
+            [
               destinationPoint2.longitude,
               destinationPoint2.latitude,
             ],
@@ -188,7 +85,7 @@ class PastLocationMarker extends React.Component<Props> {
     });
     const shapeGeoJson = {
       type: 'FeatureCollection',
-      features: [...newArrows2, ...lines],
+      features: [...arrows],
     };
 
     return (
