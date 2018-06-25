16841749b0034c0dd37f220efa86b16146e290ee
Oleg <olegwn@gmail.com>
2018-06-25 20:11:12 +0300

    Integrate lottie animation

--- app/components/map/block-mapview-component.js
+++ app/components/map/block-mapview-component.js
@@ -2,8 +2,10 @@ import * as React from 'react';
 import {
   StyleSheet,
   View,
-  ActivityIndicator,
+  // ActivityIndicator,
+  Animated,
 } from 'react-native';
+import LottieView from 'lottie-react-native';
 
 import { H2, H3 } from '../../components/ui-kit/atoms/typography';
 import {
@@ -11,22 +13,58 @@ import {
   SCREEN_HEIGHT,
 } from '../../constants';
 
+type State = {
+  animationProgress: any,
+}
+
 type Props = {
   appState: any,
 };
 
-export default class BlockMapViewComponent extends React.Component<Props> {
+export default class BlockMapViewComponent extends React.Component<Props, State> {
+  animation;
+  constructor(props) {
+    super(props);
+    this.state = {
+      animationProgress: new Animated.Value(0),
+    };
+  }
+
+  componentDidMount() {
+    this.cycleAnimation();
+  }
+
+  cycleAnimation() {
+    Animated.sequence([
+      Animated.timing(this.state.animationProgress, {
+        toValue: 1,
+        duration: 4000,
+      }),
+      Animated.timing(this.state.animationProgress, {
+        toValue: 0,
+        duration: 4000,
+      }),
+    ]).start(() => {
+      this.cycleAnimation();
+    });
+  }
+
   render() {
     return (
       <View style={styles.blockView}>
         <View style={styles.blockModal}>
           <H2>Плохой сигнал GPS ({Math.floor(this.props.appState.gpsAccuracy)})</H2>
           <H3>Выйди на открытую местность.</H3>
-          <ActivityIndicator
+          <LottieView
+            source={require('../../assets/lottie-animations/location-search.json')} // eslint-disable-line
+            progress={this.state.animationProgress}
+            style={styles.animation}
+          />
+          {/* <ActivityIndicator
             style={styles.acitityIndicator}
             size="large"
             color="gray"
-          />
+          /> */}
         </View>
       </View>
     );
@@ -61,7 +99,7 @@ const styles = StyleSheet.create({
     right: 0,
     width: SCREEN_WIDTH * 0.9,
     height: 150,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     shadowColor: 'rgba(0, 0, 0, 0.1)',
     backgroundColor: 'rgba(255,255,255,1)',
     borderRadius: 4,
@@ -71,5 +109,18 @@ const styles = StyleSheet.create({
       width: 0, height: 4,
     },
     elevation: 1,
+  },
+  animation: {
+    left: 0,
+    right: 0,
+    bottom: 0,
+    top: -10,
+    // justifyContent: 'center',
+    // alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    // width: 250,
+    height: 250,
   },
 });
